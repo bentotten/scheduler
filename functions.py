0153f26a10a58dd251b64d1f2caa24333b66fed9
Ben Totten <40713173+BenHurricane@users.noreply.github.com>
2020-06-12 20:08:31 -0700

    Merging db.py

--- functions.py
+++ functions.py
@@ -2,8 +2,44 @@
 
 # Author: Ben Hurricane
 # Origin: 12 Nov 19
-# functions.py
-# Cleaning up some of the auxilary functions out of the main py file
-# Referenced:
-# - https://realpython.com/how-to-make-a-discord-bot-python/
-# - discordpy.readthedocs.io/en/latest/api.html#discord.User.mention
+# db.py
+# sqlite specific functions
+
+import sqlite3
+from bot.py import Game
+
+db = 'roster.db'    # Roster name
+table_name = 'games'
+conn = sqlite3.connect(db)  # Creates file
+c = conn.cursor()   # Sets cursor
+
+
+# Connect
+def create_table():
+
+    c.execute(f"""CREATE TABLE [IF NOT EXISTS] {table_name} (
+                id INTEGER NOT NULL PRIMARY KEY,
+                confirmed ,
+                not_attending text
+                )""")   # Create game table
+
+    conn.commit()   # Commits current action
+
+    conn.close()    # Closes connection
+
+
+# Insert
+def insert_table(key):
+    c.execute(f"INSERT INTO {table_name} VALUES ({key}, 'Justin Wood', 'John Smith')")
+    conn.commit()
+
+
+# Query
+def query_games(key):
+    c.execute(f"SELECT * FROM games WHERE id='{key}'")
+    print(c.fetchall())
+
+
+# create_table()  # Creates table
+# insert_table('111')
+query_games('111')
